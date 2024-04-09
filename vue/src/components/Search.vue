8a98037b148e00309b569db7e0ba22a9d9bcb1cc
Megan Sharretts <megsharretts@gmail.com>
2024-04-09 10:35:38 -0400

    working on search functionality

--- vue/src/components/Search.vue
+++ vue/src/components/Search.vue
@@ -1,1 +1,21 @@
 <!-- search component for searching landmarks. Want filtering ability for types of attractions? -->
+<template>
+    
+</template>
+
+<script>
+export default {
+    data() {
+        search: {
+
+            firstName: '',
+            lastName: '',
+            username: '',
+            emailAddress: '',
+            status: ''
+        }
+
+    },
+}
+
+</script>
