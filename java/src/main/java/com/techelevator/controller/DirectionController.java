7a01d5de3e10ddf5d9f38ffa84e2e91f3f93d907
Toby Gershon <tobygershon@gmail.com>
2024-04-09 23:03:17 -0400

    wokring on directions controller but not finished

--- java/src/main/java/com/techelevator/controller/DirectionController.java
+++ java/src/main/java/com/techelevator/controller/DirectionController.java
@@ -1,12 +1,15 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.Itinerary.ItineraryDao;
+import com.techelevator.dao.Landmarks.LandmarkDao;
+import com.techelevator.dao.Landmarks.Model.Landmark;
 import com.techelevator.dao.Tour.Model.Route;
 import com.techelevator.dao.Tour.Model.Tour;
 import com.techelevator.dao.Tour.TourDao;
 import com.techelevator.service.DirectionsService;
 import com.techelevator.service.models.Directions;
 import com.techelevator.service.models.DirectionsDTO;
+import com.techelevator.service.models.Legs;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,17 +23,18 @@ import java.util.List;
 public class DirectionController {
 
     // need to get list of directions for each route in a tour
-    private final DirectionsService directionsService;
+    private final DirectionsService directionsService = new DirectionsService();
     private final ItineraryDao itineraryDao;
     private final TourDao tourDao;
+    private final LandmarkDao landmarkDao;
 
-    public DirectionController(DirectionsService directionsService, ItineraryDao itineraryDao, TourDao tourDao) {
-        this.directionsService = directionsService;
+    public DirectionController(ItineraryDao itineraryDao, TourDao tourDao, LandmarkDao landmarkDao) {
         this.itineraryDao = itineraryDao;
         this.tourDao = tourDao;
+        this.landmarkDao = landmarkDao;
     }
 
-    @GetMapping("/itineraries/{itineraryId}/directions")
+    @GetMapping("/directions/{itineraryId}")
     public DirectionsDTO getTourDirectionsList(@PathVariable int itineraryId) {
         DirectionsDTO newDTO = new DirectionsDTO();
 
@@ -38,15 +42,25 @@ public class DirectionController {
 
         Tour tour = tourDao.getTourById(tourId);
 
-        List<Route> tourRoutesList = new ArrayList<>();
+        List<Directions> tourRoutesList = new ArrayList<>();
 
         for (Route route : tour.getRoutes()) {
-            if (route)
+            if (route != null) {
+                Landmark start = landmarkDao.getLandmarkById(route.getStartingPointId());
+                Landmark end = landmarkDao.getLandmarkById(route.getStartingPointId());
+
+                Directions newDirections = directionsService.getDirections(start.getGooglePlaceId(), end.getGooglePlaceId());
+                tourRoutesList.add(newDirections);
+            }
         }
 
+        Legs[] legs = new Legs[tourRoutesList.size()];
 
-        Directions directions1 = directionsService.getDirections();
-        Directions route2 =
+        for (int i = 0; i < tourRoutesList.size(); i++) {
+            legs[i] = tourRoutesList.get(i).getRoutes()[0].getLegs()[0];
+        }
+
+        newDTO.setLegs(legs);
 
         return newDTO;
     }
