2a04816f7374c9018c4e9a8701c2bffb6db85fbf
Andie Fullmer <amfullmer@gmail.com>
2024-04-15 11:13:06 -0400

    toasted uninstalled

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="capstone-app">
     <div id="nav">
-      
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'login' }" v-if="$store.state.token == ''">Login</router-link>
       <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
-      &nbsp;|&nbsp;<router-link v-bind:to="{ name: 'itineraryList' }" v-if="$store.state.token != ''">My Itineraries</router-link>
-      &nbsp;|&nbsp;<router-link :to="{ name: 'userRating' }"  v-if="$store.state.token != ''">My Ratings</router-link>
+      &nbsp;|&nbsp;<router-link v-bind:to="{ name: 'itineraryList' }" v-if="$store.state.token != ''">My
+        Itineraries</router-link>
+      &nbsp;|&nbsp;<router-link :to="{ name: 'userRating' }" v-if="$store.state.token != ''">My Ratings</router-link>
     </div>
     <div id="main">
       <router-view />
